a66dee48db07f03a8f18dedb7be114e7e259360c
faisalahmed <faisal.ahmedbcse@gmail.com>
2021-08-22 12:38:29 +0600

    remote api calling added

--- lib/data/datasources/remote_datasource/remote_datasource.dart
+++ lib/data/datasources/remote_datasource/remote_datasource.dart
@@ -1,6 +1,14 @@
 import 'dart:io';
 
+import 'package:coding_test/data/models/new_arrivals_products_response.dart';
+import 'package:coding_test/data/models/stories_response.dart';
+import 'package:coding_test/data/models/trending_products_response.dart';
+import 'package:coding_test/data/models/trending_seller_response.dart';
+
 
 abstract class RemoteDataSource {
-
+  Future<TrendingSellerResponse> getTrendingSellers();
+  Future<TrendingProductsResponse> getTrendingProducts();
+  Future<NewArrivalsProductsResponse> getNewArrivalProducts();
+  Future<StoriesResponse> getStories();
 }
--- lib/data/datasources/remote_datasource/remote_datasource_impl.dart
+++ lib/data/datasources/remote_datasource/remote_datasource_impl.dart
@@ -1,7 +1,10 @@
-import 'dart:io';
 import 'package:coding_test/core/network/api_base_helper.dart';
+import 'package:coding_test/data/models/new_arrivals_products_response.dart';
+import 'package:coding_test/data/models/stories_response.dart';
+import 'package:coding_test/data/models/trending_products_response.dart';
+import 'package:coding_test/data/models/trending_seller_response.dart';
+import 'package:coding_test/utils/constant/constant.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'remote_datasource.dart';
 
 
@@ -11,5 +14,31 @@ class RemoteDataSourceImpl extends RemoteDataSource{
   final ApiBaseHelper? helper;
 
   RemoteDataSourceImpl({@required this.helper});
-  
+
+  @override
+  Future<NewArrivalsProductsResponse> getNewArrivalProducts() async{
+    var response = await helper!.get(NetworkConstants.NEW_ARRIVALS);
+    return NewArrivalsProductsResponse.fromJson(response.data);
+  }
+
+  @override
+  Future<StoriesResponse> getStories() async{
+    var response = await helper!.get(NetworkConstants.NEW_ARRIVALS);
+    return StoriesResponse.fromJson(response.data);
+  }
+
+  @override
+  Future<TrendingProductsResponse> getTrendingProducts() async{
+    var response = await helper!.get(NetworkConstants.NEW_ARRIVALS);
+    return TrendingProductsResponse.fromJson(response.data);
+  }
+
+  @override
+  Future<TrendingSellerResponse> getTrendingSellers() async{
+    var response = await helper!.get(NetworkConstants.NEW_ARRIVALS);
+    return TrendingSellerResponse.fromJson(response.data);
+  }
+
+
+
 }
